de6ec09eb6651c2f59671e3209cd458090221b72
SerjMartin <serjmartin@mail.com>
2021-11-06 19:23:19 +0000

    Add testApply_extention test

--- student.py
+++ student.py
@@ -1,9 +1,11 @@
 from datetime import date, timedelta
 
+
 class Student:
     """ A Student class as a basee for method testing """
+
     def __init__(self, first_name, last_name):
-        self._first_name  = first_name
+        self._first_name = first_name
         self._last_name = last_name
         self._start_date = date.today()
         self.end_date = date.today() + timedelta(days=365)
@@ -19,3 +21,6 @@ class Student:
 
     def alert_santa(self):
         self.naughty_list = True
+
+    def apply_extension(self, days):
+        self.end_date += timedelta(days = days)
